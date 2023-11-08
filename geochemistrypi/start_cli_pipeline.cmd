3f841f1120d1876a17ecfc3ea94b9d33697b1212
Sunjianhao <97781484+Jin-sjh@users.noreply.github.com>
2023-11-08 20:48:14 +0800

    2023/11/8 3种打包方式整理

---
+++ geochemistrypi/start_cli_pipeline.cmd
@@ -0,0 +1,6 @@
+
+@echo off
+rem This script was created by Nuitka to execute 'start_cli_pipeline.exe' with Python DLL being found.
+set PATH=c:\users\dell\appdata\local\programs\python\PYTHON~1;%PATH%
+set PYTHONHOME=C:\Users\DELL\AppData\Local\Programs\Python\Python310
+"%~dp0start_cli_pipeline.exe" %*
